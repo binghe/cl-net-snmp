68d9217cb732095b03468269f8198eaf7858c1a6
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-01-29 08:38:27 +0000

    Try to recover MIB support

--- snmp/trunk/devel.lisp
+++ snmp/trunk/devel.lisp
@@ -1,7 +1,10 @@
-(in-package :asn.1)
+(in-package :snmp)
 
 (defun update-syntax (&optional (zb *asn.1-syntax-source*) (tab *asn.1-syntax*))
   (let ((*warn-conflicts* t)
         (*allow-conflicts* t))
     (zebu-compile-file zb :output-file tab)
     (zebu-load-file tab)))
+
+(eval-when (:load-toplevel :execute)
+  (update-syntax))
