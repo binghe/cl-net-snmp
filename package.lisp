767054173b4e5314b36485a69a540d888f81ea18
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-02-19 09:26:59 +0000

    Add get-next and bulk support

--- package.lisp
+++ package.lisp
@@ -15,18 +15,23 @@
            #:with-open-session
            ;; snmp operation
    	   #:snmp-get
+           #:snmp-get-next
            #:snmp-walk
+           #:snmp-get-bulk
            ;; utils
            #:mib-browser
            #:snmp-utility))
 
 (in-package :snmp)
 
-(let* ((defaults (asdf:component-pathname (asdf:find-system :snmp)))
-       (home (make-pathname :name :wild :type :wild
-                            :directory (append (pathname-directory defaults)
-                                               '(:wild-inferiors))
-                            :host (pathname-host defaults)
-                            :defaults defaults)))
-  (setf (logical-pathname-translations "snmp")
-        `(("**;*.*" ,home))))
+;;; Logical Pathname Translations
+(eval-when (:load-toplevel :execute)
+  (let* ((defaults #+asdf (asdf:component-pathname (asdf:find-system :snmp))
+                   #-asdf *load-pathname*)
+         (home (make-pathname :name :wild :type :wild
+                              :directory (append (pathname-directory defaults)
+                                                 '(:wild-inferiors))
+                              :host (pathname-host defaults)
+                              :defaults defaults)))
+    (setf (logical-pathname-translations "snmp")
+          `(("**;*.*" ,home)))))
