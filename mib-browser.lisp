2d961361a84e6428c1714bd084e94aaf24dffa5c
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-02-20 18:13:24 +0000

    MIB browser improve and other minor changes

--- mib-browser.lisp
+++ mib-browser.lisp
@@ -1,5 +1,7 @@
 (in-package :snmp)
 
+(defvar *nodes* nil)
+
 (defun children-function (x)
   (let ((children (tree-nodes x)))
     (and children children)))
@@ -10,52 +12,64 @@
           (id (car (tree-id x))))
       (format nil "~A(~D)" name id))))
 
+(defclass mib-browser-node (capi:expandable-item-pinboard-object)
+  ()
+  (:default-initargs))
+
 (capi:define-interface mib-browser ()
   ()
   (:panes
    (mib-graph capi:graph-pane
+              :node-pinboard-class 'mib-browser-node
               :children-function 'children-function
-              :directed t
               :print-function 'print-child
-              :accessor browser-graph
-              :roots (cdr (tree-nodes *mib-tree*))
+              :reader mib-browser-mib-graph
+              :roots (tree-nodes *mib-tree*)
               :callback-type :interface-data
-              :selection-callback 'display-graph-selection)
-   (title-pane-name capi:title-pane
-                    :text "Name: ")
-   (display-pane-name capi:display-pane
-                      :text "")
-   (title-pane-oid capi:title-pane
-                   :text "OID: ")
-   (display-pane-oid capi:display-pane
-                     :text ""))
+              :selection-callback 'display-graph-selection
+              :visible-min-height 300)
+   (search capi:text-input-pane
+           :title "Search:"
+           :text "system"
+           :callback 'search-callback
+           :buttons (list :ok t)
+           :callback-type :interface-data
+           :visible-min-width '(:character 10)
+           :visible-max-width '(:character 30))
+   (title-pane-name capi:title-pane :text "Name (OID):")
+   (display-pane-name capi:display-pane))
   (:layouts
    (main-layout capi:column-layout
-                '(mib-graph grid-layout))
-   (grid-layout capi:grid-layout
-                '(title-pane-name display-pane-name title-pane-oid display-pane-oid)
+                '(head-layout mib-graph))
+   (head-layout capi:row-layout
+                '(search title-pane-name display-pane-name)
                 :y-adjust :center))
   (:default-initargs
-   :best-height 600
-   :best-width 800
    :layout 'main-layout
    :title "MIB Browser"))
 
-(defmethod initialize-instance :after ((interface mib-browser) &rest initargs
-                                       &key &allow-other-keys)
-  (declare (ignore initargs))
-  (let ((root (capi:graph-pane-nodes (browser-graph interface))))
-    (format t "call this function:~A~%" (type-of root))))
+(defmethod capi:interface-display :before ((self mib-browser))
+  (let ((g (mib-browser-mib-graph self))
+        (start-node (gethash "internet" *mib-index*)))
+    (setf (capi:choice-selected-item g) start-node
+          *nodes* (capi:graph-pane-nodes g))
+    (display-graph-selection self start-node)))
 
 (defun display-graph-selection (self data)
   (let ((name (reverse (tree-name data)))
         (oid (reverse (tree-id data))))
-    (with-slots (display-pane-name display-pane-oid) self
+    (with-slots (display-pane-name) self
       (setf (capi:display-pane-text display-pane-name)
-            (format nil "~A~{.~A~}" (car name) (cdr name))
-            (capi:display-pane-text display-pane-oid)
-            (format nil "~A~{.~A~}" (car oid) (cdr oid))))))
+            (format nil "~A~{.~A~} (~A~{.~A~})"
+                    (car name) (cdr name)
+                    (car oid) (cdr oid))))))
+
+(defun search-callback (interface data)
+  (let ((g (mib-browser-mib-graph interface))
+        (current-node (gethash data *mib-index*)))
+    (when current-node
+      (setf (capi:choice-selected-item g) current-node)
+      (display-graph-selection interface current-node))))
 
 (defun mib-browser ()
   (capi:display (make-instance 'mib-browser)))
-
