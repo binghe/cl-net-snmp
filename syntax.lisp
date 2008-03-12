373bbca46e5d81c1b9351abeb584da47c4eca890
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-03-12 18:42:34 +0000

    Fix zebu issue

--- syntax.lisp
+++ syntax.lisp
@@ -1,11 +1,12 @@
 (in-package :snmp)
 
-(defparameter *asn.1-source* #p"snmp:asn1;asn1.zb")
-(defparameter *asn.1-syntax* #p"snmp:asn1;asn1.tab")
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *asn.1-source* #p"snmp:asn1;asn1.zb")
+  (defparameter *asn.1-syntax* #p"snmp:asn1;asn1.tab")
 
-(defun generate-print-function (item stream level)
-  (declare (ignore item level))
-  (format stream "<GPF>"))
+  (defun generate-print-function (item stream level)
+    (declare (ignore item level))
+    (format stream "<GPF>")))
 
 (defun update-syntax (&optional (zb *asn.1-source*) (tab *asn.1-syntax*))
   (let ((*warn-conflicts* t)
@@ -13,5 +14,13 @@
     (zebu-compile-file zb :output-file tab)
     (zebu-load-file tab)))
 
+(eval-when (:compile-toplevel)
+  (let ((*warn-conflicts* t)
+        (*allow-conflicts* t))
+    (zebu-compile-file *asn.1-source* :output-file *asn.1-syntax*)))
+
 (eval-when (:load-toplevel :execute)
-  (update-syntax))
+  (let ((*warn-conflicts* t)
+        (*allow-conflicts* t))
+    (zebu-load-file *asn.1-syntax*)))
+
