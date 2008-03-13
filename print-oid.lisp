1e0547cd783e0a530c3530c6f72e38bc81ae02db
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-03-13 19:16:18 +0000

    Add :mib features to release depends on fuck ZEBU

--- print-oid.lisp
+++ print-oid.lisp
@@ -5,6 +5,7 @@
 (defparameter *oid-print-short* t)
 (defparameter *oid-print-length* 4)
 
+#+mib
 (defmethod print-object ((obj object-id) stream)
   (with-slots (rev-ids rev-names) obj
     (print-unreadable-object (obj stream :type t)
@@ -25,3 +26,11 @@
                   (cdr part-2))))
       (when (and *oid-print-id* *oid-print-name*)
         (format stream ")")))))
+
+#-mib
+(defmethod print-object ((obj object-id) stream)
+  (with-slots (rev-ids rev-names) obj
+    (print-unreadable-object (obj stream :type t)
+      (when *oid-print-id*
+        (let ((part-1 (reverse rev-ids)))
+          (format stream "~A~{.~A~}" (car part-1) (cdr part-1)))))))
