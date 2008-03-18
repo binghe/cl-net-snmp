64569c15002313af947913b5b0da05032e9f18c4
Chun Tian <binghe.lisp@gmail.com>
2008-03-18 20:07:51 +0000

    porting code to usocket-udp, lispworks client part is done.

--- network.lisp
+++ network.lisp
@@ -4,24 +4,17 @@
 
 (defgeneric send-snmp-message (session message &key))
 
-#+lispworks
 (defun send-until (action socket &key (times 6) (wait-time 2))
   (loop with result = nil
         for i from 0 below times
-        until result
+        until (car result)
         do (progn
              (when (plusp i)
                (format t "resend times: ~A at ~A.~%" i (get-internal-real-time)))
              (funcall action)
-             #-win32 (mp:notice-fd socket)
-             (setf result
-                   (mp:process-wait-with-timeout "Waiting for a socket to become active"
-                                                 wait-time
-                                                 #'(lambda () (comm::socket-listen socket))))
-             #-win32 (mp:unnotice-fd socket))
+             (setf result (wait-for-input socket :timeout wait-time)))
         finally (return result)))
 
-#+lispworks
 (defun recv-until (action stop-reason)
   (loop as message = (funcall action)
         until (funcall stop-reason message)
@@ -32,46 +25,15 @@
   (let ((socket (socket-of session))
         (data (ber-encode message)))
     (labels ((send ()
-               #+lispworks
                (progn 
-                 (write-sequence data socket)
-                 (force-output socket))
-               #+sbcl
-               (sb-bsd-sockets:socket-send socket
-                                           (coerce data '(simple-array (unsigned-byte 8) (*)))
-                                           (length data)))
+                 (write-sequence data (socket-stream socket))
+                 (force-output (socket-stream socket))))
              (recv ()
-               #+lispworks
-               (decode-message session socket)
-               #+sbcl
-               (let ((buffer (sb-bsd-sockets:socket-receive socket nil 65507)))
-                 (decode-message session (make-instance 'ber-stream
-                                                        :sequence (map 'vector #'char-code buffer))))))
+               (decode-message session (socket-stream socket))))
       (if (not receive)
         (send)
-	#+lispworks
         (progn
-          (if (send-until #'send (socket-stream-socket socket)
-                          #+ignore #'(lambda () (listen socket)))
+          (if (send-until #'send socket)
 	    (recv-until #'recv #'(lambda (x) (= (request-id-of (pdu-of message))
                                                 (request-id-of (pdu-of x)))))
-            (error "cannot got a reply")))
-	#+sbcl
-	(progn
-	  (send)
-	  (recv))))))
-
-#+sbcl
-(defun open-udp-stream (host port &key element-type &allow-other-keys)
-  (declare (type integer port)
-	   (type (or vector string) host))
-  (let ((socket (make-instance 'sb-bsd-sockets:inet-socket :type :datagram :protocol :udp
-			       :element-type element-type))
-	(address (if (stringp host)
-		     (sb-bsd-sockets:host-ent-address (sb-bsd-sockets:get-host-by-name host)) host)))
-    (sb-bsd-sockets:socket-connect socket address port)
-    socket))
-
-#+cmu
-(defun open-udp-stream (host port &key &allow-other-keys)
-  nil)
+            (error "cannot got a reply")))))))
--- usocket-udp/usocket-lispworks.lisp
+++ usocket-udp/usocket-lispworks.lisp
@@ -19,6 +19,47 @@
 
 (in-package :usocket)
 
+;;; Part One: bugfix for usocket-svn
 
+(defun usocket-listen (usocket)
+  (if (stream-usocket-p usocket)
+    (when (listen (socket-stream usocket))
+      usocket)
+    (when (comm::socket-listen (socket usocket))
+      usocket)))
+
+#-win32
+(defun wait-for-input-internal (sockets &key timeout)
+  (with-mapped-conditions ()
+    ;; unfortunately, it's impossible to share code between
+    ;; non-win32 and win32 platforms...
+    ;; Can we have a sane -pref. complete [UDP!?]- API next time, please?
+    (mapcar #'(lambda (x) (mp:notice-fd (os-socket-handle x))) sockets)
+    (mp:process-wait-with-timeout "Waiting for a socket to become active"
+                                  (truncate timeout)
+                                  #'(lambda (socks)
+                                      (some #'usocket-listen socks))
+                                  sockets)
+    (mapcar #'(lambda (x) (mp:unnotice-fd (os-socket-handle x))) sockets)
+    (remove nil (mapcar #'usocket-listen sockets))))
+
+(defun socket-connect/udp (host port &key (stream nil) (element-type 'base-char))
+  (if (and host port)
+    (if stream
+      (let ((stream (comm:open-udp-stream host port
+                                          :element-type element-type
+                                          :errorp nil)))
+        (make-stream-datagram-socket
+         (comm:socket-stream-socket stream) stream))
+      (make-datagram-socket (comm:connect-to-udp-server host port :errorp nil)))
+    (make-datagram-socket (comm:open-udp-socket :errorp nil))))
+
+(defmethod socket-send ((socket datagram-usocket) buffer length &key address port)
+  (let ((s (socket socket)))
+    (comm:send-message s buffer length address port)))
+
+(defmethod socket-receive ((socket datagram-usocket) buffer length)
+  (let ((s (socket socket)))
+    (comm:receive-message s buffer length)))
 
 :eof
