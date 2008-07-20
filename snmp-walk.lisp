5f2627746b2dc55c11254e416895687f080a922d
Chun Tian <binghe.lisp@gmail.com>
2008-07-20 06:38:25 +0000

    [snmp] 3.0 prerelease, all files stable now. part 2

--- snmp-walk.lisp
+++ snmp-walk.lisp
@@ -1,18 +1,3 @@
-;;;; -*- Mode: lisp; Syntax: ansi-common-lisp; Base: 10; Package: snmp; -*-
-
-#|
-<DOCUMENTATION>
- <DESCRIPTION>
-  SNMP GET-NEXT-REQUEST and high-level "WALK" support
-  </DESCRIPTION>
- <COPYRIGHT YEAR='2007-2008' AUTHOR='Chun Tian (binghe)' MARK='(C)'
-            HREF='https://cl-net-snmp.svn.sourceforge.net/svnroot/cl-net-snmp/snmp/trunk/snmp-walk.lisp'/>
- <CHRONOLOGY>
-  <DELTA DATE='20080316'>create documentation for "snmp-walk.lisp"</DELTA>
-  </CHRONOLOGY>
- </DOCUMENTATION>
-|#
-
 (in-package :snmp)
 
 (defgeneric snmp-get-next (object vars &key)
@@ -26,7 +11,7 @@
 (defmethod snmp-get-next ((session session) (vars list) &key (context ""))
   "SNMP Get-Next-Request for v1, v2c and v3"
   (when vars
-    (let ((vb (mapcar #'(lambda (x) (list (*->oid x) nil)) vars)))
+    (let ((vb (mapcar #'(lambda (x) (list (oid x) nil)) vars)))
       ;; Get a report first if the session is new created
       (when (and (= +snmp-version-3+ (version-of session))
                  (need-report-p session))
@@ -57,7 +42,7 @@
 (defmethod snmp-walk ((session session) (vars list) &key (context ""))
   "SNMP Walk for v1, v2c and v3"
   (when vars
-    (let ((base-vars (mapcar #'*->oid vars)))
+    (let ((base-vars (mapcar #'oid vars)))
       (labels ((iter (current-vars acc)
                  (let* ((temp (snmp-get-next session current-vars :context context))
                         (new-vars (mapcar #'first temp))
@@ -73,5 +58,3 @@
 
 (defmethod snmp-walk (object (var object-id) &key (context ""))
   (car (snmp-walk object (list var) :context context)))
-
-:eof
