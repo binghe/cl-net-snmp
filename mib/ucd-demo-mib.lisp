95c373562d372c9e41b9193f6806f9c73a6765d5
Chun Tian <binghe.lisp@gmail.com>
2008-07-20 07:05:34 +0000

    [snmp] 3.0 prerelease, part 4

---
+++ mib/ucd-demo-mib.lisp
@@ -0,0 +1,11 @@
+;;;; Auto-generated from ASN-SNMP:UCD-DEMO-MIB
+
+(IN-PACKAGE :ASN.1)
+(SETF *CURRENT-MODULE* 'UCD-DEMO-MIB)
+(DEFOID |ucdDemoMIB| (|ucdavis| 14))
+(DEFOID |ucdDemoMIBObjects| (|ucdDemoMIB| 1))
+(DEFOID |ucdDemoPublic| (|ucdDemoMIBObjects| 1))
+(DEFOID |ucdDemoResetKeys| (|ucdDemoPublic| 1))
+(DEFOID |ucdDemoPublicString| (|ucdDemoPublic| 2))
+(DEFOID |ucdDemoUserList| (|ucdDemoPublic| 3))
+(DEFOID |ucdDemoPassphrase| (|ucdDemoPublic| 4))
