3161ddff30d687593e9b421b8f5402f9ac000397
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-07-28 05:13:55 +0000

    [snmp] minor bugfix.

--- snmp/trunk/network.lisp
+++ snmp/trunk/network.lisp
@@ -14,7 +14,7 @@
                                       (wait-time *snmp-wait-timeout*))
   (loop with result = nil
         for i from 0 below times
-        until (car result)
+        until result
         do (progn
              (when (plusp i)
                (format t "resend times: ~A at ~A.~%" i (get-internal-real-time)))
