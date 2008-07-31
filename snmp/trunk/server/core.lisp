c301e058fad455a0fc6e4215ba487cd2c503c8e7
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-07-31 11:41:57 +0000

    [snmp] fix build order and oid presentation in def-scalar-variable

--- snmp/trunk/server/core.lisp
+++ snmp/trunk/server/core.lisp
@@ -5,20 +5,20 @@
 (defvar *snmp-server-contact* "Chun Tian (binghe) <binghe.lisp@gmail.com>")
 
 ;;; system tree
-(def-scalar-variable |sysDescr| (o)
+(def-scalar-variable "sysDescr" (o)
   (format nil "~A ~A"
           (lisp-implementation-type) (lisp-implementation-version)))
 
-(def-scalar-variable |sysContact| (o)
+(def-scalar-variable "sysContact" (o)
   *snmp-server-contact*)
 
-(def-scalar-variable |sysName| (o)
+(def-scalar-variable "sysName" (o)
   (long-site-name))
 
-(def-scalar-variable |sysLocation| (o)
+(def-scalar-variable "sysLocation" (o)
   (machine-instance))
 
-(def-scalar-variable |sysORLastChange| (o)
+(def-scalar-variable "sysORLastChange" (o)
   (timeticks 0))
 
 ;;; |sysORID|, |sysORDescr|, |sysORUpTime| from 1 to 9 is not implemented.
