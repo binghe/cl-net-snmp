5ffcf0ad238431e69e40fa9c4c6a04c0f113f7ab
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-08-01 15:42:38 +0000

    [snmp] optimize, IANA number added.

--- constants.lisp
+++ constants.lisp
@@ -16,7 +16,24 @@
 (defconstant +snmp-sec-level-authnopriv+ 2)
 (defconstant +snmp-sec-level-authpriv+   3)
 
-;;; from snmp_client.h
-(defconstant +snmp-stat-success+ 0)
-(defconstant +snmp-stat-error+ 1)
-(defconstant +snmp-stat-timeout+ 2)
+;;; Following constants are come from SYSMAN/Lisp-SNMP project
+
+(defconstant +error-status-no-error+		  0)
+(defconstant +error-status-too-big+		  1)
+(defconstant +error-status-no-such-name+	  2) ;for proxy compatibility
+(defconstant +error-status-bad-value+		  3) ;for proxy compatibility
+(defconstant +error-status-read-only+		  4) ;for proxy compatibility
+(defconstant +error-status-generic-error+	  5)
+(defconstant +error-status-no-access+		  6)
+(defconstant +error-status-wrong-type+		  7)
+(defconstant +error-status-wrong-length+	  8)
+(defconstant +error-status-wrong-encoding+	  9)
+(defconstant +error-status-wrong-value+		 10)
+(defconstant +error-status-no-creation+		 11)
+(defconstant +error-status-inconsistent-value+	 12)
+(defconstant +error-status-resource-unavailable+ 13)
+(defconstant +error-status-commit-failed+	 14)
+(defconstant +error-status-undo-failed+		 15)
+(defconstant +error-status-authorization-error+	 16)
+(defconstant +error-status-not-writable+	 17)
+(defconstant +error-status-inconsistent-name+	 18)
