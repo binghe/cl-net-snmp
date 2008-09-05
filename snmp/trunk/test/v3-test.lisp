17efd50ec6ea22658122a08e16eb4f9ffebbd737
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-09-05 18:37:45 +0000

    [snmp] fix for lispworks/win32, add some basic tests.

--- snmp/trunk/test/v3-test.lisp
+++ snmp/trunk/test/v3-test.lisp
@@ -3,16 +3,16 @@
 
 (in-package :snmp)
 
-(defun snmp-test-1 ()
-  (snmp:with-open-session (s "2950.lab.163.org"
-                             :version :v3
-                             :user "md5"
-                             :auth "vHunxJXPRdUyAzjY")
-    (snmp:snmp-get s "sysDescr.0")))
+(defun v3-test-1 ()
+  (with-open-session (s "2950.lab.163.org"
+                        :version :v3
+                        :user "md5"
+                        :auth "vHunxJXPRdUyAzjY")
+    (snmp-get s "sysDescr.0")))
 
-(defun snmp-test-2 ()
-  (snmp:with-open-session (s "localhost"
-                             :version :v3
-                             :user "readonly"
-                             :auth "ABCDEFGHABCDEFGH")
-    (snmp:snmp-get s "sysContact.0")))
+(defun v3-test-2 ()
+  (with-open-session (s "localhost"
+                        :version :v3
+                        :user "readonly"
+                        :auth "ABCDEFGHABCDEFGH")
+    (snmp-get s "sysContact.0")))
