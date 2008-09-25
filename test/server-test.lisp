a067e20025416507cf28f36412a87498da2898ea
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-09-25 19:00:06 +0000

    [snmp-server] 3.1 patch: snmp-walk fix when walk to the end of whole mib tree.

--- test/server-test.lisp
+++ test/server-test.lisp
@@ -17,5 +17,13 @@
                           :port *default-snmp-server-port*
                           :version :2c
                           :community "public") ; actually community could be any string
-      (snmp-walk s "common-lisp"))))
+      (snmp-walk s "lispSystem"))))
 
+;; This cannot work in snmp-server-3.0, fixed in 3.1
+(defun server-test-3 ()
+  (with-open-snmp-server ()
+    (with-open-session (s (or *default-snmp-server-address* "localhost")
+                          :port *default-snmp-server-port*
+                          :version :2c
+                          :community "public") ; actually community could be any string
+      (snmp-walk s "common-lisp"))))
