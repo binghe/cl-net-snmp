56a2e1f252fa949a5eea8c4e0908b74719953af7
Chun Tian <binghe.lisp@gmail.com>
2008-09-27 12:21:31 +0000

    [snmp] 5.11: SNMPv3 fix: resend message on report.

--- network.lisp
+++ network.lisp
@@ -38,54 +38,39 @@
                           :address (host-of session)
                           :port (port-of session))))))
 
-(defmethod send-snmp-message ((session v3-session) (message v3-message) &key (receive t) (report nil))
+(defmethod send-snmp-message ((session v3-session) (message v3-message) &key (receive t))
   "this new send-snmp-message is just a interface,
    all UDP retransmit code are moved into usocket-udp project."
   (cond (receive ; normal message
-         #-(and lispworks win32)
-         (socket-sync (socket-of session) message
-                      :address (host-of session)
-                      :port (port-of session)
-                      :encode-function #'(lambda (x)
-                                           (values (coerce (ber-encode x) 'octets)
-                                                   (msg-id-of x)))
-                      :decode-function #'(lambda (x)
-                                           (let ((m (decode-message session x)))
-                                             (values m (msg-id-of m))))
-                      :max-receive-length +max-snmp-packet-size+)
-         #+(and lispworks win32)
-         (comm:sync-message (socket (socket-of session)) ; raw socket fd
-                            message
-                            (host-of session)
-                            (port-of session)
-                            :encode-function #'(lambda (x)
-                                                 (values (coerce (ber-encode x) 'octets)
-                                                         (msg-id-of x)))
-                            :decode-function #'(lambda (x)
-                                                 (let ((m (decode-message session x)))
-                                                   (values m (msg-id-of m))))
-                            :max-receive-length +max-snmp-packet-size+))
-        ;; report message: send and get raw response
-        ((and (not receive) report)
-         #-(and lispworks win32)
-         (socket-sync (socket-of session) message
-                      :address (host-of session)
-                      :port (port-of session)
-                      :encode-function #'(lambda (x)
-                                           (values (coerce (ber-encode x) 'octets) 0))
-                      :max-receive-length +max-snmp-packet-size+)
-         #+(and lispworks win32)
-         (comm:sync-message (socket (socket-of session)) ; raw socket fd
-                            message
-                            (host-of session)
-                            (port-of session)
-                            :encode-function #'(lambda (x)
-                                                 (values (coerce (ber-encode x) 'octets) 0))
-                            :max-receive-length +max-snmp-packet-size+))
+         (labels ((encode-function (x)
+                    (values (coerce (ber-encode x) 'octets)
+                            (msg-id-of x)))
+                  (decode-function (x)
+                    (let ((m (decode-message session x)))
+                      (values m (msg-id-of m))))
+                  (send ()
+                    #-(and lispworks win32)
+                    (socket-sync (socket-of session) message
+                                 :address (host-of session)
+                                 :port (port-of session)
+                                 :encode-function #'encode-function
+                                 :decode-function #'decode-function
+                                 :max-receive-length +max-snmp-packet-size+)
+                    #+(and lispworks win32)
+                    (comm:sync-message (socket (socket-of session)) ; raw socket fd
+                                       message
+                                       (host-of session)
+                                       (port-of session)
+                                       :encode-function #'encode-function
+                                       :decode-function #'decode-function
+                                       :max-receive-length +max-snmp-packet-size+)))
+           (let ((reply-message (send)))
+             (if (report-flag-of reply-message)
+               (send) ; send again when got a snmp report
+               reply-message))))
         ;; trap message: only send once
-        ((and (not receive) (not report))
-         (let* ((data (coerce (ber-encode message) 'octets))
-                (data-length (length data)))
-           (socket-send (socket-of session) data data-length
-                        :address (host-of session)
-                        :port (port-of session))))))
+        (t (let* ((data (coerce (ber-encode message) 'octets))
+                  (data-length (length data)))
+             (socket-send (socket-of session) data data-length
+                          :address (host-of session)
+                          :port (port-of session))))))
--- package.lisp
+++ package.lisp
@@ -60,7 +60,7 @@
           `(("**;*.*.NEWEST" "SNMP:ASN;**;*.*")
             ("**;*.*" "SNMP:ASN;**;*.*")))))
 
-(defparameter *version* 5.10)
+(defparameter *version* 5.11)
 (defparameter *server-version* 3.3)
 
 (eval-when (:load-toplevel :execute)
