41a16af41f1904181938a9b40a8844ad19af2b90
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-09-27 07:48:24 +0000

    [snmp] patch 5.9: compare msg-id (not request-id anymore) when sending snmpv3 messages, this is a preparation for detect report pdu when sending any snmpv3 messages.

--- snmp/trunk/patch/snmp-5/5-9.lisp
+++ snmp/trunk/patch/snmp-5/5-9.lisp
@@ -1,8 +1,36 @@
-;;;; -*- Mode: Lisp -*-
-;;;; $Id$
+;;;; Patch 5.9: better SNMPv3 support
 
 (in-package :snmp)
 
+;;; Remove unused function
+(fmakunbound 'generate-salt)
+(unintern 'generate-salt)
+
+;;; New DECODE-MESSAGE will also decode MsgID part
+(defmethod decode-message ((s v3-session) (message-list list))
+  (destructuring-bind (version global-data security-string data) message-list
+    (declare (ignore version))
+    (let ((msg-id (elt global-data 0)))
+      (if (not (priv-protocol-of s))
+        (let ((pdu (elt data 2)))
+          (make-instance 'v3-message :session s :id msg-id :pdu pdu))
+        ;;; decrypt message
+        (let ((salt (map 'octets #'char-code
+                         (elt (ber-decode<-string security-string) 5)))
+              (des-key (subseq (priv-local-key-of s) 0 8))
+              (pre-iv (subseq (priv-local-key-of s) 8 16))
+              (data (map 'octets #'char-code data)))
+          (let* ((iv (map 'octets #'logxor pre-iv salt))
+                 (cipher (ironclad:make-cipher :des ; (priv-protocol-of s)
+                                               :mode :cbc
+                                               :key des-key 
+                                               :initialization-vector iv)))
+            (ironclad:decrypt-in-place cipher data)
+            (let ((pdu (elt (ber-decode data) 2)))
+              (make-instance 'v3-message :session s :id msg-id :pdu pdu))))))))
+
+;; change SEND-SNMP-MESSAGE from defun to defgeneric
+(fmakunbound 'send-snmp-message)
 (defgeneric send-snmp-message (session message &key))
 
 (defmethod send-snmp-message ((session v1-session) (message v1-message) &key (receive t))
@@ -89,3 +117,5 @@
            (socket-send (socket-of session) data data-length
                         :address (host-of session)
                         :port (port-of session))))))
+
+(defparameter *version* 5.9)
