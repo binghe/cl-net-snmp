9d5fe6d3a996fcc9a3e82db167aa7ba7a0de04b6
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-09-28 08:03:37 +0000

    [snmp] patch 5.12: MIB change, delete 3 old MIB files.

--- mib/rfc-1215.lisp
+++
@@ -1,7 +0,0 @@
-;;;; -*- Mode: Lisp -*-
-;;;; Auto-generated from MIB:NET-SNMP;RFC-1215.TXT
-
-(in-package :asn.1)
-(setf *current-module* 'rfc-1215)
-(eval-when (:load-toplevel :execute) (pushnew 'rfc-1215 *mib-modules*))
-(defmacro trap-type ())
