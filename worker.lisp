e294f8385ec5a1569e7f5888fcac0738c31ae244
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-10-06 07:51:35 +0000

    [snmp] commit for new branch (5)

--- worker.lisp
+++ worker.lisp
@@ -3,22 +3,9 @@
 
 (in-package :snmp)
 
-(defvar *send-worker-cv* (make-condition-variable))
-(defvar *recv-worker-cv* (make-condition-variable))
+;;; SNMP-WORKER is a low-level API for sync lots of SNMP messages once.
 
-(defun send-worker ()
-  (loop (thread-yield)
-        (sleep 1)))
-
-(defun recv-worker ()
-  (loop (thread-yield)
-        (sleep 1)))
-
-(defclass snmp-operation ()
-  ((session :type session
-            :accessor session-of)
-   (oper    :type (member :get :report :set :get-next)
-            :accessor oper-of)
-   (args    :type list
-            :accessor args-of))
-  (:documentation "Single SNMP operation"))
+(defun snmp-worker (op-list)
+  (declare (type list op-list)) ; each item is (session . pdu-list)
+  0
+  )
