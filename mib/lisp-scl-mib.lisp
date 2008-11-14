06b8cf327561cc10fd076efb2ea7b51bfd87eebd
Chun Tian <binghe.lisp@gmail.com>
2008-11-14 17:24:33 +0000

    [snmp] multiple patch from branch 5, clean package.lisp.

---
+++ mib/lisp-scl-mib.lisp
@@ -0,0 +1,10 @@
+;;;; -*- Mode: Lisp -*-
+;;;; Auto-generated from SNMP:SERVER;MIB;LISP-SCL-MIB.TXT
+
+(in-package :asn.1)
+(setf *current-module* 'lisp-scl-mib)
+(eval-when (:load-toplevel :execute)
+  (pushnew 'lisp-scl-mib *mib-modules*))
+(defoid |scl| (|common-lisp| 10)
+  (:type 'module-identity)
+  (:description "The MIB module for Scieneer CL"))
