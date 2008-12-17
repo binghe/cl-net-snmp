98cd9c3244866e8f04e5a386f6ab02cf1cce03da
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2008-12-17 07:09:59 +0000

    [snmp] 5.21: restore CREATE-SOCKET in OPEN-SESSION.

--- snmp/branches/5/package.lisp
+++ snmp/branches/5/package.lisp
@@ -61,7 +61,7 @@
             ("**;*.*" "SNMP:ASN;**;*.*")))))
 
 (defparameter *major-version* 5)
-(defparameter *minor-version* 20)
+(defparameter *minor-version* 21)
 
 (defparameter *server-major-version* 3)
 (defparameter *server-minor-version* 12)
