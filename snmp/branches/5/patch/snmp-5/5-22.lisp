2cd6bf38f96457675a7b946af09d7e12854ad00a
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2009-01-21 09:07:33 +0000

    [snmp] fix for snmp-walk termination test, thank to MSI

--- snmp/branches/5/patch/snmp-5/5-22.lisp
+++ snmp/branches/5/patch/snmp-5/5-22.lisp
@@ -1,14 +1,7 @@
-;;;; -*- Mode: Lisp -*-
-;;;; $Id$
+;;;; Handle terminal condition correctly, just detect duplicate return OIDs.
+;;;; Thanks for John Fremlin from MSI
 
 (in-package :snmp)
-
-(defgeneric snmp-walk (object vars &key &allow-other-keys)
-  (:documentation "SNMP Walk, only useful for debug"))
-
-(defmethod snmp-walk ((host string) (vars list) &key context)
-  (with-open-session (s host)
-    (snmp-walk s vars :context context)))
 
 (defun walk-terminate-p (new-vars current-vars base-vars)
   (or (some #'oid->= new-vars base-vars)
@@ -29,9 +22,3 @@
                          (mapcar #'nreverse acc))
                      (iter new-vars (mapcar #'cons temp acc) nil)))))
         (iter base-vars (make-list (length vars)) t)))))
-
-(defmethod snmp-walk (object (var string) &key context)
-  (car (snmp-walk object (list var) :context context)))
-
-(defmethod snmp-walk (object (var object-id) &key context)
-  (car (snmp-walk object (list var) :context context)))
