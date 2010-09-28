9ff641586852fc642bf4055056ad38d99c45b110
binghe <binghe@b4d60608-ca3d-0410-9b8f-f4facadce3c5>
2010-09-28 16:53:37 +0000

    SNMP: fixed build of application bundle on Mac OS X (with a trivial icon)

--- snmp/trunk/deliver.lisp
+++ snmp/trunk/deliver.lisp
@@ -8,6 +8,11 @@
 #+linux
 (require "capi-ps-lib")
 
+#+:cocoa
+(compile-file-if-needed
+  (sys:example-file "configuration/macos-application-bundle")
+                   :load t)
+
 ;;; Load the "application".
 (load *init-file-name*)
 
@@ -20,7 +25,16 @@
           asn.1:*mib-modules*))
 
 ;;; Deliver the MIB Browser
-(deliver 'snmp-ui:mibrowser #p"SNMP:DIST;MIBROWSER.EXE" 4
+(deliver 'snmp-ui:mibrowser
+         #+mswindows #p"SNMP:DIST;MIBROWSER.EXE"
+         #+linux #p"SNMP:DIST;MIBROWSER"
+         #+cocoa (write-macos-application-bundle
+                  (make-pathname :name "MIB Browser"
+                                 :defaults (translate-logical-pathname #p"SNMP:DIST;"))
+                  :executable-name "mibrowser"
+                  :version "6.0"
+                  :application-icns #p"SNMP:INTERFACE;ICONS;MIBROWSER.ICNS")
+         4
          :interface :capi
          :keep-pretty-printer t
          :kill-dspec-table nil
