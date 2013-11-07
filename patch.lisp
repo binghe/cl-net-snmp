82581b96888cb072522bfa91fd8c9912229f746f
Chun Tian <binghe.lisp@gmail.com>
2013-11-07 03:10:29 +0000

    Created tag 6.0.3.

--- patch.lisp
+++ patch.lisp
@@ -59,12 +59,10 @@
        do (load-one-patch-file file)
        count file into count
        finally
-	 (format t
-	         *patch-format-string*
-		     count
-		     (string-upcase (asdf:component-name system))
-             major-version
-             (+ minor-version count))
+	 (format t *patch-format-string*
+		 count (string-upcase (asdf:component-name system))
+                 major-version (+ minor-version count))
+         #+ignore
 	 (unless (zerop count)
 	   (setf (asdf:component-version system)
 		 (format nil "~D.~D" major-version (+ minor-version count)))))))
